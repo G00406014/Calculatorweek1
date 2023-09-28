a39d21837164a8693bd7a91bce37884f7e45c2ad
g00406014 <G00406014@atu.ie>
2023-09-28 22:32:00 +0100

    Initial commit

---
+++ src/main/java/org/example/CalCulationResults.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class CalCulationResults {
+    private final String operation;
+    private final int total;
+
+    public CalCulationResults(String operation, int total) {
+        this.operation = operation;
+        this.total = total;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+}
